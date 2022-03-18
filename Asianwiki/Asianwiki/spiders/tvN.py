6b200cec0e09891ac54ff996905d0636de02ee4b
Stephen Atehe <atehestephen@gmail.com>
2022-03-18 20:34:45 +0100

    setup template for tvN spider

--- Asianwiki/Asianwiki/spiders/tvN.py
+++ Asianwiki/Asianwiki/spiders/tvN.py
@@ -1,10 +1,11 @@
 import scrapy
+from scrapy.utils.response import open_in_browser
 
 
 class TvnSpider(scrapy.Spider):
-    name = 'tvN'
-    allowed_domains = ['www.asianwiki.com']
-    start_urls = ['http://www.asianwiki.com/']
+    name = "tvN"
+    allowed_domains = ["www.asianwiki.com"]
+    start_urls = ["https://www.asianwiki.com/Category:TvN_Drama_Series"]
 
     def parse(self, response):
-        pass
+        open_in_browser(response)
