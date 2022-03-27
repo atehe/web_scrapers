bdaf883ce4b0e247a13788274a5b5a2681251c43
Stephen Atehe <atehestephen@gmail.com>
2022-03-27 22:16:50 +0100

    add pagination

--- Jumia/Jumia/settings.py
+++ Jumia/Jumia/settings.py
@@ -38,8 +38,20 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+#     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#     "Accept-Encoding": "gzip, deflate, br",
+#     "Accept-Language": "en-US,en;q=0.9",
+#     "Host": "httpbin.org",
+#     "Sec-Ch-Ua": '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+#     "Sec-Ch-Ua-Mobile": "?0",
+#     "Sec-Ch-Ua-Platform": '"Windows"',
+#     "Sec-Fetch-Dest": "document",
+#     "Sec-Fetch-Mode": "navigate",
+#     "Sec-Fetch-Site": "none",
+#     "Sec-Fetch-User": "?1",
+#     "Upgrade-Insecure-Requests": "1",
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36",
+#     "X-Amzn-Trace-Id": "Root=1-623f97e1-5f2679d835f220ad12ad1782",
 # }
 
 # Enable or disable spider middlewares
@@ -81,7 +93,7 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+# HTTPCACHE_ENABLED = True
 # HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
--- Jumia/Jumia/spiders/flashsales.py
+++ Jumia/Jumia/spiders/flashsales.py
@@ -1,19 +1,22 @@
 import scrapy
-
+import logging
 
 class FlashsalesSpider(scrapy.Spider):
     name = "flashsales"
     allowed_domains = ["www.jumia.com.ng"]
     start_urls = ["http://www.jumia.com.ng/flash-sales/"]
+    page_num = 1
 
     def parse(self, response):
+        logging.info(f'\nSCRAPING PAGE {self.page_num}\n')
+        self.page_num += 1
+        
         products = response.xpath("//section/div/article")
 
         for product in products:
             product_url = product.xpath(".//a/@href").get()
             product_name = product.xpath(".//a/@data-name").get()
-            print(product_url)
-
+            
             if product_url == None:
                 continue
 
@@ -21,6 +24,13 @@ class FlashsalesSpider(scrapy.Spider):
                 url=product_url,
                 callback=self.parse_product,
                 meta={"product_name": product_name},
+            )
+        
+        next_page = response.xpath("//a[@aria-label='Next Page']/@href").get()
+        if next_page:
+            yield response.follow(
+                url=next_page,
+                callback=self.parse
             )
 
     def parse_product(self, response):
