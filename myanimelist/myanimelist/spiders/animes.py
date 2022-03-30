dbcc4ba8304af435ee2c81f26a36bdb191f480e0
Stephen Atehe <atehestephen@gmail.com>
2022-03-30 16:14:32 +0100

    refactor code for cleaner output

--- myanimelist/myanimelist/spiders/animes.py
+++ myanimelist/myanimelist/spiders/animes.py
@@ -3,6 +3,20 @@ import cfscrape
 from scrapy.utils.response import open_in_browser
 
 
+def clean_xpath_data(data):
+    invalid_data = [",", "\n", ""]
+    data = [elem.strip().strip("\n").strip(",") for elem in data]
+    valid_data = [elem for elem in data if elem not in invalid_data]
+    info_header = [elem[:-1] for elem in valid_data if elem[-1] == ":"]
+    if len(info_header) == 1:
+        info_name = info_header[0]
+        if info_name == "Theme":
+            info_name = "Themes"
+        info_data_list = set(valid_data[1:])
+        info_data = ", ".join(info_data_list)
+        return info_name, info_data
+
+
 class AnimesSpider(scrapy.Spider):
     name = "animes"
     allowed_domains = ["myanimelist.net"]
@@ -33,94 +47,18 @@ class AnimesSpider(scrapy.Spider):
             )
 
     def parse_anime(self, response):
-
         name = response.request.meta["name"]
+        info_dict = {"Name": name}
 
-        yield {
-            "Name": name,
-            "Type": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[1]/a/text()"
-            ).get(),
-            "Episodes": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[2]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Status": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[3]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Aired": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[4]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Premiered": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[5]/a/text()"
-            ).get(),
-            "Broadcast": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[6]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Producers": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[7]/a/text()"
-            ).getall(),
-            "Licensors": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[8]/a/text()"
-            ).get(),
-            "Studios": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[9]/a/text()"
-            ).get(),
-            "Source": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[10]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Genres": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[11]/a/text()"
-            ).getall(),
-            "Themes": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[12]/a/text()"
-            ).getall(),
-            "Demographic": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[13]/a/text()"
-            ).get(),
-            "Duration": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[14]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Rating": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[15]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Score": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[16]/span[2]/text()"
-            ).get(),
-            "Scored By": response.xpath(
-                "//h2[text()='Information']/following-sibling::div[16]/span[3]/text()"
-            ).get(),
-            "Ranked": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[17]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Popularity": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[18]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Members": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[19]/text()"
-                ).getall()
-            ).strip("\n "),
-            "Favorites": "".join(
-                response.xpath(
-                    "//h2[text()='Information']/following-sibling::div[20]/text()"
-                ).getall()
-            ).strip("\n "),
-        }
+        info_xpaths = response.xpath(
+            "//h2[text()='Information']/following-sibling::div"
+        )
+
+        for info in info_xpaths:
+            data = info.xpath(".//text()").getall()
+            info_data = clean_xpath_data(data)
+            if not info_data:
+                continue
+            info_dict[info_data[0]] = info_data[1]
+
+        yield info_dict
