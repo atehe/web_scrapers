04a8680e76cbf8f4c00c8dd430e0a82748de0a92
Stephen Atehe <atehestephen@gmail.com>
2022-04-10 17:22:00 +0100

    render js with splash

--- worldometer/worldometer/spiders/covid19_case.py
+++ worldometer/worldometer/spiders/covid19_case.py
@@ -1,10 +1,49 @@
 import scrapy
+from scrapy_splash import SplashRequest
+from scrapy.utils.response import open_in_browser
 
 
 class Covid19CaseSpider(scrapy.Spider):
-    name = 'covid19_case'
-    allowed_domains = ['www.worldometers.info']
-    start_urls = ['http://www.worldometers.info/']
+    name = "covid19_case"
+    allowed_domains = ["www.worldometers.info"]
+
+    script = """
+    function main(splash, args)
+        assert(splash:go(args.url))
+        assert(splash:wait(10))
+        splash:set_viewport_full()
+        return {
+            html = splash:html()
+        }
+    end 
+    """
+
+    def start_requests(self):
+        yield SplashRequest(
+            url="https://www.worldometers.info/coronavirus",
+            callback=self.parse,
+            endpoint="execute",
+            args={"lua_source": self.script},
+        )
 
     def parse(self, response):
-        pass
+        print(response.body)
+
+
+# script = """
+#     function main(splash, args)
+#         assert(splash:go(args.url))
+#         assert(splash:wait(10))
+#         splash:set_viewport_full()
+#         return {
+#             html = splash:html()
+#         }
+#     end
+#     """
+# print(
+#     SplashRequest(
+#         url="https://www.worldometers.info/coronavirus",
+#         endpoint="execute",
+#         args={"lua_source": script},
+#     )
+# )
